bf3a58de9d101557c5988601265f924be0e1bd1a
Anna Mylordis <annamylordis@gmail.com>
2019-10-27 15:40:31 +1100

    #anna - changed shutdown interval

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ import random
 import time
 
 UPDATE_INTERVAL = 25 # In ms, this comes about to ~40 frames a second
-SHUTDOWN_INTERVAL = 4300 # in ms
+SHUTDOWN_INTERVAL = 4500 # in ms
 
 def run_animation():
     wrapper = ClientWrapper()
