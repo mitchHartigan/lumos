562b021b3b619313d2c406dd43a384122e330faf
Anna Mylordis <annamylordis@gmail.com>
2019-10-29 17:01:15 +1100

    Revert "#anna - WIP animation stop when pot value changed"
    This reverts commit 0ffa1a24790aec713c678a597ee34b6599872ef0.

--- index.py
+++ index.py
@@ -2,7 +2,6 @@ from dmx_test import SimpleFadeController
 from array import array
 from ola.ClientWrapper import ClientWrapper
 from gpiozero import MCP3008
-from time import sleep
 import random
 import time
 
@@ -26,19 +25,8 @@ def run_strip_animation():
     controller = None
 
 if __name__ == '__main__':
-    pot_val = pot.value
-    while True:
-        if pot_val == pot.value:
-            # nothing has changed.
-            i = 0
-            while i < 10:
-                run_strip_animation()
-                i += 1
-        else:
-            # the value has changed.
-            pot_val = pot.value
-            ClientWrapper.Stop()
-            time.sleep(1)
-
-
+    i = 0
+    while i < 10:
+        run_strip_animation()
+        i += 1
 
