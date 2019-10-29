c7d0600f5f91e902fc6da548542b4a1a3b995fe5
Anna Mylordis <annamylordis@gmail.com>
2019-10-29 13:30:17 +1100

    #anna - WIP reading pot input

--- index.py
+++ index.py
@@ -1,11 +1,14 @@
 from dmx_test import SimpleFadeController
 from array import array
 from ola.ClientWrapper import ClientWrapper
+from gpiozero import MCP3008
 import random
 import time
 
 UPDATE_INTERVAL = 25 # In ms, this comes about to ~40 frames a second
 SHUTDOWN_INTERVAL = 5200 # in ms
+
+pot = MCP3008(0)
 
 UNIVERSE_1 = 1
 UNIVERSE_2 = 2
@@ -14,6 +17,7 @@ UNIVERSE_3 = 3
 def run_strip_animation():
     wrapper = ClientWrapper()
     controller = SimpleFadeController(UPDATE_INTERVAL, wrapper)
+    print(pot.value)
     wrapper.AddEvent(SHUTDOWN_INTERVAL, wrapper.Stop)
     wrapper.Run()
 
