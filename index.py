4af3854f1dd73f70a55ee5d3b5ff034e886014ac
Anna Mylordis <annamylordis@gmail.com>
2019-10-29 17:05:58 +1100

    #anna - WIP stop animation when pot value is changed

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from dmx_test import SimpleFadeController
 from array import array
 from ola.ClientWrapper import ClientWrapper
 from gpiozero import MCP3008
+from time import sleep
 import random
 import time
 
@@ -25,5 +26,19 @@ def run_strip_animation():
     controller = None
 
 if __name__ == '__main__':
+    pot_val = pot.value
     while True:
-      print(pot.value)
+        if pot_val == pot.value:
+            # nothing has changed.
+            i = 0
+            while i < 10:
+                run_strip_animation()
+                i += 1
+        else:
+            # the value has changed.
+            pot_val = pot.value
+            ClientWrapper.Stop()
+            time.sleep(1)
+
+
+
