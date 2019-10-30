97170410a74dcccf41e5279538ed7a82be131482
JX <jcao0081@uni.sydney.edu.au>
2019-10-30 18:35:27 +1100

    #JX - first try with pots after gradient works

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from dmx_test import SimpleFadeController
 from array import array
 from ola.ClientWrapper import ClientWrapper
+from gpiozero import MCP3008
+from time import sleep
 import random
 import time
 
@@ -22,7 +24,16 @@ def run_strip_animation():
     controller = None
 
 if __name__ == '__main__':
-    i = 0
-    while i < 10:
-        run_strip_animation()
-        i += 1
+    pot_val = pot.value
+     while True:
+         if pot_val == pot.value:
+            # nothing has changed.
+            i = 0
+            while i < 10:
+                run_strip_animation()
+                i += 1
+        else:
+            # the value has changed.
+            pot_val = pot.value
+            ClientWrapper.Stop()
+            time.sleep(1) 
