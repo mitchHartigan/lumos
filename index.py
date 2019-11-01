6342e2d761e7a90cfe3105630ff5da51c77cd2a4
JX <jcao0081@uni.sydney.edu.au>
2019-11-01 17:57:54 +1100

    #JX - not running animation, revert back to test if it works

--- index.py
+++ index.py
@@ -26,16 +26,21 @@ def run_strip_animation():
     controller = None
 
 if __name__ == '__main__':
-    pot_val = pot.value
-    while True:
-        pot_val *= 100
-        if pot_val == pot.value + 1 or pot_val == pot.value - 1: #value fluctuated so would never satisfy "=="
-            # pot has been untouched, value is the same
-            run_strip_animation()
-        else:
-            # pot has been turned, value has changed
-            pot_val = pot.value
-            # stop the system
+    i = 0
+    while i < 10:
+        run_strip_animation()
+        i += 1
+    #not running any animation for some reason
+    # pot_val = pot.value
+    # while True:
+    #     pot_val *= 100
+    #     if pot_val == pot.value + 1 or pot_val == pot.value - 1: #value fluctuated so would never satisfy "=="
+    #         # pot has been untouched, value is the same
+    #         run_strip_animation()
+    #     else:
+    #         # pot has been turned, value has changed
+    #         pot_val = pot.value
+    #         # stop the system
 
     
  #while true fucked the code...
