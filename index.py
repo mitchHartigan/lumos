d44f3fcbe39618714cda4f21f2cd6ff5ad1d151b
Anna Mylordis <annamylordis@gmail.com>
2019-11-02 01:51:17 +1100

    #anna - cleaning up unused code

--- index.py
+++ index.py
@@ -9,20 +9,11 @@ import time
 UPDATE_INTERVAL = 25 # In ms, this comes about to ~40 frames a second
 SHUTDOWN_INTERVAL = 5200 # in ms
 
-# UNIVERSE_1 = 1
-# UNIVERSE_2 = 2
-# UNIVERSE_3 = 3
-
 def run_strip_animation():
     wrapper = ClientWrapper()
     controller = SimpleFadeController(UPDATE_INTERVAL, wrapper)
-    # if pot_val <= pot_val + 2 and pot_val >= pot_val - 2:
     wrapper.AddEvent(SHUTDOWN_INTERVAL, wrapper.Stop)
     wrapper.Run()
-    # else:
-    #     print(pot_val)
-    #     wrapper.Stop()
-    #     pot_val_unchanged = False
 
     # Clears the variables, to prevent scope pollution
     wrapper = None
@@ -30,32 +21,4 @@ def run_strip_animation():
 
 if __name__ == '__main__':
     while True:
-    # while pot_val_unchanged:
         run_strip_animation()
-
-    #not running any animation for some reason
-    # pot_val = pot.value
-    # while True:
-    #     pot_val *= 100
-    #     if pot_val == pot.value + 1 or pot_val == pot.value - 1: #value fluctuated so would never satisfy "=="
-    #         # pot has been untouched, value is the same
-    #         run_strip_animation()
-    #     else:
-    #         # pot has been turned, value has changed
-    #         pot_val = pot.value
-    #         # stop the system
-
-    
- #while true fucked the code...
-    # pot_val = pot.value * 100
-    # while True:
-    #     if pot_val == pot.value:
-    #         # nothing has changed.
-    #         i = 0
-    #         while i < 10:
-    #             run_strip_animation(wrapper)
-    #             i += 1
-    #     else:
-    #         # the value has changed.
-    #         pot_val = pot.value
-    #         ClientWrapper.Stop
