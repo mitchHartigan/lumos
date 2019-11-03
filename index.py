9d5d1733d2ff21045571b70fe37793c145519e13
mitchhartigan <mitchhartigan@gmail.com>
2019-11-03 23:34:15 +1100

    #mitch - increased shutdown interval

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ import random
 import time
 
 UPDATE_INTERVAL = 20 # In ms, this comes about to ~40 frames a second
-SHUTDOWN_INTERVAL = 5800 # in ms
+SHUTDOWN_INTERVAL = 6000 # in ms
 
 def run_strip_animation():
     wrapper = ClientWrapper()
