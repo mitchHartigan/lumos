4f68f0f3cd1fa39dbb96a016df2c7b31e4ffa7c6
mitchhartigan <mitchhartigan@gmail.com>
2019-11-03 16:55:47 +1100

    #mitch - tweaking update interval

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ import random
 import time
 
 UPDATE_INTERVAL = 25 # In ms, this comes about to ~40 frames a second
-SHUTDOWN_INTERVAL = 4600 # in ms
+SHUTDOWN_INTERVAL = 4500 # in ms
 
 def run_strip_animation():
     wrapper = ClientWrapper()
