45db56281db2bfbdcd166348d0601e17811344ba
mitchhartigan <mitchhartigan@gmail.com>
2019-11-03 23:09:29 +1100

    #mitch - removed new_value declaration

--- dmx_test.py
+++ dmx_test.py
@@ -154,8 +154,6 @@ class SimpleFadeController(object):
 
         if pot_val >= 25:
             gradient = self.gradient5
-
-        print(pot_val)
             
         """
         This function gets called periodically based on UPDATE_INTERVAL
@@ -191,7 +189,7 @@ class SimpleFadeController(object):
                 # if not at 65 iterations, the strip isn't full yet, and therefore is still ascending.
                 # Adds a pixel to the array if so.  
 
-                new_value = self.read_values(universe_one_offset, gradient)
+                # new_value = self.read_values(universe_one_offset, gradient)
 
                 # self._universe_one_array.extend(new_value)
                 # self._universe_one_array.extend(new_value)
@@ -219,7 +217,7 @@ class SimpleFadeController(object):
                     self._universe_two_data_length -= 3
 
             else:    
-                new_value = self.read_values(universe_two_offset, gradient)
+                # new_value = self.read_values(universe_two_offset, gradient)
 
                 # self._universe_two_array.extend(new_value)  
 
@@ -244,7 +242,7 @@ class SimpleFadeController(object):
                 if self._universe_three_data_length > 3:
                     self._universe_three_data_length -= 3
             else:    
-                new_value = self.read_values(universe_three_offset, gradient)
+                # new_value = self.read_values(universe_three_offset, gradient)
 
                 # self._universe_three_array.extend(new_value)
 
@@ -270,7 +268,7 @@ class SimpleFadeController(object):
                     self._universe_four_data_length -= 6
 
             else:
-                new_value = self.read_values(universe_four_offset, gradient)
+                # new_value = self.read_values(universe_four_offset, gradient)
 
                 # self._universe_four_array.extend(new_value)
                 # self._universe_four_array.extend(new_value)
