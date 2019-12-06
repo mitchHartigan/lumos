786f19dbb93ff373fbfa208080a3484f35b751f9
mitchhartigan <mitchhartigan@gmail.com>
2019-12-06 14:45:40 +1100

    #mitch - added debug statements

--- controller.py
+++ controller.py
@@ -121,19 +121,19 @@ class SimpleFadeController(object):
 
         # Uses the potentiometer value to select different color gradients.
         if pot_val >= 0 and pot_val <= 10:
-          self._universe_one_array = array('B', self.genRedToOrange(pot_val, 180))
-          self._universe_two_array = array('B', self.genRedToOrange(pot_val, 180))
-          self._universe_three_array = array('B', self.genRedToOrange(pot_val, 180))
-          self._universe_four_array = array('B', self.genRedToOrange(pot_val, 180))
-          print(self._universe_one_array)
+          # self._universe_one_array = array('B', self.genRedToOrange(pot_val, 180))
+          # self._universe_two_array = array('B', self.genRedToOrange(pot_val, 180))
+          # self._universe_three_array = array('B', self.genRedToOrange(pot_val, 180))
+          # self._universe_four_array = array('B', self.genRedToOrange(pot_val, 180))
+          # print(self._universe_one_array)
 
         if pot_val >= 11 and pot_val <= 20:
           # makes the pot_val range from 1-10 for ez
           # multiplication, instead of 11-20.
           pot_val = pot_val - 10
 
-          self._universe_one_array = array('B', self.genOrangeToYellow(pot_val, 180))
-          print(self._universe_one_array)
+          # self._universe_one_array = array('B', self.genOrangeToYellow(pot_val, 180))
+          # print(self._universe_one_array)
 
         if pot_val >= 10 and pot_val < 15:
           print('')
@@ -162,6 +162,7 @@ class SimpleFadeController(object):
 
         # Send each array, a frame of animation, to each respective universe.
         self._client.SendDmx(1, self._universe_one_array)
+        print(self._universe_one_array)
         self._client.SendDmx(2, self._universe_two_array)
         self._client.SendDmx(3, self._universe_three_array)
         self._client.SendDmx(4, self._universe_four_array)
