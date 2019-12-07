8e2f6c19bb2396f328e2a3efbccd03644495fd02
Mitchell Hartigan <mitch.hartigan@gmail.com>
2019-12-07 19:18:43 +1100

    #mitch - added debugging statements

--- controller.py
+++ controller.py
@@ -11,20 +11,6 @@ class SimpleFadeController(object):
         self._wrapper = client_wrapper
         self._client = client_wrapper.Client()
         self._wrapper.AddEvent(self._update_interval, self.UpdateDmx)
-        self._iterable = 1
-
-        # Initialize the unique array for each strip.
-        self._universe_one_array = array('B', [])
-        self._universe_two_array = array('B', [])
-        self._universe_three_array = array('B', [])
-        self._universe_four_array = array('B', [])
-
-        # Initialize a data length for each strip.
-        # These will update at different rates, as the strips remove array elements at different rates.
-        self._universe_one_data_length = 360
-        self._universe_two_data_length = 180
-        self._universe_three_data_length = 180
-        self._universe_four_data_length = 360
 
     def fillStripWithColor(self, rgb_color_arr, length):
       i = 1
@@ -36,8 +22,6 @@ class SimpleFadeController(object):
 
     def genRedToOrange(self,pot_val, length):
       strip_arr = []
-      if pot_val <= 0:
-        pot_val = 1
 
       pot_val = (pot_val/4)
       green_val = round((pot_val*15))
@@ -52,9 +36,6 @@ class SimpleFadeController(object):
     def genOrangeToYellow(self, pot_val, length):
       strip_arr = []
 
-      if pot_val <= 0:
-        pot_val = 1
-
       pot_val = (pot_val/4)
       green_val = round( (150 + (pot_val * 10)) )
       green_val = int(green_val)
@@ -67,8 +48,6 @@ class SimpleFadeController(object):
 
     def genYellowToGreen(self, pot_val, length):
       strip_arr = []
-      if pot_val <= 0:
-        pot_val = 1
 
       pot_val = (pot_val/4)
       red_val = round( (250 - (pot_val * 25)) ) 
@@ -82,8 +61,6 @@ class SimpleFadeController(object):
 
     def genGreenToAqua(self, pot_val, length):
       strip_arr = []
-      if pot_val <= 0:
-        pot_val = 1
     
       pot_val = (pot_val/4)
       blue_val = round((25 * pot_val))
@@ -97,8 +74,6 @@ class SimpleFadeController(object):
 
     def genAquaToBlue(self, pot_val, length):
       strip_arr = []
-      if pot_val <= 0:
-        pot_val = 1
 
       pot_val = (pot_val/4)
       green_val = round( (250 - (pot_val * 25)) )
@@ -112,8 +87,6 @@ class SimpleFadeController(object):
 
     def genBlueToPink(self, pot_val, length):
       strip_arr = []
-      if pot_val <= 0:
-        pot_val = 1
       
       pot_val = (pot_val/4)
       red_val = round((pot_val * 25))
@@ -127,8 +100,6 @@ class SimpleFadeController(object):
 
     def genPinkToRed(self, pot_val, length):
       strip_arr = []
-      if pot_val <= 0:
-        pot_val = 1
       
       pot_val = (pot_val/4)
       blue_val = round( (250 - (pot_val * 25)) )
@@ -148,33 +119,35 @@ class SimpleFadeController(object):
 
         # Uses the potentiometer value to select different color gradients.
         if pot_val >= 0 and pot_val <= 40:
-          if pot_val == 0:
-            pot_val = 1
-          
-          self._universe_one_array = array('B', self.genGreenToAqua(pot_val, 190))
+
+          self._universe_one_array = array('B', self.genGreenToAqua(pot_val, 180))
           self._universe_two_array = array('B', self.genPinkToRed(pot_val, 60))
           self._universe_three_array = array('B', self.genBlueToPink(pot_val, 180))
           self._universe_four_array = array('B', self.genYellowToGreen(pot_val, 180))
+          print(self._universe_four_array)
+          print('Length of universe 4: ', len(self._universe_four_array))
 
         if pot_val >= 41 and pot_val <= 80:
           # Keep the pot_val between 1 and 20, for ez multiplication.
           pot_val = int(pot_val - 40)
 
-          self._universe_one_array = array('B', self.genAquaToBlue(pot_val, 190))
+          self._universe_one_array = array('B', self.genAquaToBlue(pot_val, 180))
           self._universe_two_array = array('B', self.genRedToOrange(pot_val, 60))
           self._universe_three_array = array('B', self.genPinkToRed(pot_val, 180))
           self._universe_four_array = array('B', self.genGreenToAqua(pot_val, 180))
+          print(self._universe_four_array)
+          print('Length of universe 4: ', len(self._universe_four_array))
 
         if pot_val >= 81 and pot_val <= 100:
           # Keep the pot_val between 1 and 20, for ez multiplication.
           pot_val = pot_val - 80
 
-          self._universe_one_array = array('B', self.genBlueToPink(pot_val, 190) )
+          self._universe_one_array = array('B', self.genBlueToPink(pot_val, 180) )
           self._universe_two_array = array('B', self.genOrangeToYellow(pot_val, 60))
           self._universe_three_array = array('B', self.genRedToOrange(pot_val, 180))
           self._universe_four_array = array('B', self.genAquaToBlue(pot_val, 180))
-
-        self._iterable += 1
+          print(self._universe_four_array)
+          print('Length of universe 4: ', len(self._universe_four_array))
 
         # Send each array, a frame of animation, to each respective universe.
         self._client.SendDmx(1, self._universe_one_array)
