485a7c2a41eab1f8569d5999c0db5e16347acbe6
mitchhartigan <mitchhartigan@gmail.com>
2019-12-07 14:34:54 +1100

    #mitch - debug, removed rounding to check type error

--- controller.py
+++ controller.py
@@ -39,8 +39,7 @@ class SimpleFadeController(object):
       if pot_val <= 0:
         pot_val = 1
 
-      pot_val = (pot_val/2)
-      green_val = round((pot_val*15), 0)
+      green_val = 150
       
       rgb_color_arr = [ 255, green_val, 0 ]
 
